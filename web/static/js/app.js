bb6d7b1846bd4d2ec8bbecdfb030b77afa21eeca
Maria Silla <maria.sylla@gmail.com>
2017-05-06 17:56:19 -0400

    youtube api

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,52 +11,126 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
+
 import "phoenix_html"
 import {Socket,Presence} from "phoenix"
+import $ from "jquery"
 
-let user = document.getElementById('user').innerText;
-let socket = new Socket("/socket", {params: {user: user}})
-socket.connect()
+$(function() {
+  let user = document.getElementById('user').innerText;
+  let socket = new Socket("/socket", {params: {user: user}})
+  socket.connect()
 
-let presence = {}
-let formatedTimestamp = (Ts) => {
-  let date = new Date(Ts)
-  return date.toLocaleString()
-}
-
-let listBy = (user, {metas: metas}) => {
-  return {
-    user: user,
-    onlineAt: formatedTimestamp(metas[0].online_at)
+  let presence = {}
+  let formatedTimestamp = (Ts) => {
+    let date = new Date(Ts)
+    return date.toLocaleString()
   }
+
+  let listBy = (user, {metas: metas}) => {
+    return {
+      user: user,
+      onlineAt: formatedTimestamp(metas[0].online_at)
+    }
+  }
+
+  let userlist = document.getElementById('userlist');
+  let render = (presences) => {
+    userlist.innerHTML = Presence.list(presences, listBy)
+    .map(presence => `
+      <li>
+        <strong>${presence.user}</strong>
+        <br>
+          online at: ${presence.onlineAt}
+        </li>
+        `)
+        .join("")
+      }
+
+
+
+      let room = socket.channel("room:lobby")
+      room.on("presence_state", state => {
+        presence = Presence.syncState(presence,state)
+        render(presence)
+      })
+      room.on("presence_diff", diff => {
+        presence = Presence.syncDiff(presence, diff)
+        render(presence)
+      })
+
+      room.join()
+
+      //YouTube API
+      $('form').on("submit", function(e){
+        e.preventDefault();
+        // the request
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            publishedAfter: "2015-01-01T00:00:00Z"
+        });
+        //execute the request
+        request.execute(function(response){
+          console.log(response);
+
+        })
+      });
+
+});
+
+//YouTube api
+function init() {
+  gapi.client.setApiKey(API KEY GOES HERE);
+  gapi.client.load("youtube", "v3", function(){
+  });
 }
 
-let userlist = document.getElementById('userlist');
-let render = (presences) => {
-  userlist.innerHTML = Presence.list(presences, listBy)
-  .map(presence => `
-    <li>
-     <strong>${presence.user}</strong>
-     <br>
-     online at: ${presence.onlineAt}
-     </li>
-    `)
-    .join("")
-}
+//YouTube API
+//This code loads the IFrame Player API code asynchronously.
+//  var tag = document.createElement('script');
+//  tag.src = "https://www.youtube.com/iframe_api";
+//       var firstScriptTag = document.getElementsByTagName('script')[0];
+//       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// //This function creates an <iframe> (and YouTube player) after the API code downloads.
+// var player;
+//       function onYouTubeIframeAPIReady() {
+//         player = new YT.Player('player', {
+//           height: '390',
+//           width: '640',
+//           videoId: 'M7lc1UVf-VE',
+//           events: {
+//             'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange
+//           }
+//         });
+//       }
+// //The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+//         event.target.playVideo();
+//       }
+// // The API calls this function when the player's state changes.
+//       //    The function indicates that when playing a video (state=1),
+//       //    the player should play for six seconds and then stop.
+//       var done = false;
+//             function onPlayerStateChange(event) {
+//               if (event.data == YT.PlayerState.PLAYING && !done) {
+//                 setTimeout(stopVideo, 6000);
+//                 done = true;
+//               }
+//             }
+//             function stopVideo() {
+//               player.stopVideo();
+//             }
+//
 
 
 
-let room = socket.channel("room:lobby")
-room.on("presence_state", state => {
-  presence = Presence.syncState(presence,state)
-  render(presence)
-})
-room.on("presence_diff", diff => {
-  presence = Presence.syncDiff(presence, diff)
-  render(presence)
-})
 
-room.join()
+
 // Import local files
 //
 // Local files can be imported directly using relative
