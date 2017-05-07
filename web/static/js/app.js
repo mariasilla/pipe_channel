41e6b48a1f5c34a1a3c86d79aa403cab5cce3293
Maria Silla <maria.sylla@gmail.com>
2017-05-06 21:42:20 -0400

    add souncloud playlist widget

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -61,33 +61,57 @@ $(function() {
 
       room.join()
 
-      //YouTube API
-      $('form').on("submit", function(e){
-        e.preventDefault();
-        // the request
-        var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 3,
-            order: "viewCount",
-            publishedAfter: "2015-01-01T00:00:00Z"
-        });
-        //execute the request
-        request.execute(function(response){
-          console.log(response);
+    //Soundcloud Api Widget
+    (function(){
+      var widgetIframe = document.getElementById('sc-widget'),
+          widget       = SC.Widget(widgetIframe);
 
-        })
+      widget.bind(SC.Widget.Events.READY, function() {
+        widget.bind(SC.Widget.Events.PLAY, function() {
+          // get information about currently playing sound
+          widget.getCurrentSound(function(currentSound) {
+            console.log('sound ' + currentSound.get('') + 'began to play');
+          });
+        });
+        // get current level of volume
+        widget.getVolume(function(volume) {
+          console.log('current volume value is ' + volume);
+        });
+        // set new volume level
+        widget.setVolume(50);
+        // get the value of the current position
       });
 
-});
+    }());
 
-//YouTube api
-function init() {
-  gapi.client.setApiKey(API KEY GOES HERE);
-  gapi.client.load("youtube", "v3", function(){
-  });
-}
+
+      //YouTube API
+//       $('form').on("submit", function(e){
+//         e.preventDefault();
+//         // the request
+//         var request = gapi.client.youtube.search.list({
+//             part: "snippet",
+//             type: "video",
+//             q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+//             maxResults: 3,
+//             order: "viewCount",
+//             publishedAfter: "2015-01-01T00:00:00Z"
+//         });
+//         //execute the request
+//         request.execute(function(response){
+//           console.log(response);
+//
+//         })
+//       });
+//
+});
+//
+// //YouTube api
+// function init() {
+//   gapi.client.setApiKey(API KEY GOES HERE);
+//   gapi.client.load("youtube", "v3", function(){
+//   });
+// }
 
 //YouTube API
 //This code loads the IFrame Player API code asynchronously.
